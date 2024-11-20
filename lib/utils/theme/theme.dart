4944a873ed2d91bd168d22018c9fb6f0949fc810
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-19 23:41:49 -0400

    feat: Add elevated buttons theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rarevault_ecommerce/utils/theme/text_theme.dart';
+import 'package:rarevault_ecommerce/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
