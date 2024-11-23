ea992c2904e59eab20fa410870478f1cc652aebc
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-11-23 12:17:56 -0400

    feat: Initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rarevault_ecommerce/app.dart';
+import 'package:rarevault_ecommerce/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
